a267380223be5bf2f85fa94cd6ca11e24eb4f8f8
Asim Mittal <asim.mittal@gmail.com>
2016-10-11 19:36:10 -0400

    some more changes on handling text input

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 
 var gMap = require("./gmap");
 var helpers = require("./helpers");
+var map;
 
 // this array will contain all the polygons that may be drawn on this
 // google map
@@ -18,7 +19,7 @@ gMap.load(function () {
 	
 	//let's init our map object and draw something
 	//in the DOM element with id 'map'
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14
         , center: initLocation
         , mapTypeId: 'terrain'
@@ -29,7 +30,8 @@ gMap.load(function () {
     var textBox = document.getElementById("txtData");
 	var loader = document.getElementById("progLoader");
     
-	//Button clicked
+	/// 1 - Button clicked
+	/// This triggers everything
 	button.onclick = function () {
 		
 		//let's validate what the user's entered
@@ -44,7 +46,7 @@ gMap.load(function () {
 			//all the entered zips are valid. Let's do our thing now...
 			//GET the /zipcode? route in our local server
 			//once the request completes, handle the response
-			var url = "/zipcode?" + "92122";
+			var url = "/zipcode?" + lstZipCodes.join("&");
 			helpers.httpGetAsync(url,handleResponse,1000);
 		}
 		else{
@@ -52,7 +54,8 @@ gMap.load(function () {
 		}
 	}
 	
-	//handle response of GET call
+	/// 2 - handle response of GET call
+	/// Server's responded, process the response
 	var handleResponse = function(req){
 		loader.hide();
 		if(req.status == 200){
@@ -65,18 +68,25 @@ gMap.load(function () {
 			var latCenter = response.meta.latCenter;
 			var lngCenter = response.meta.lngCenter;
 			
-			//draw these polygons
+			//pan the map to the center coordinates
+			map.panTo(new google.maps.LatLng(latCenter, lngCenter));
+			
+			//draw these polygons. Timeout used to allow the map to pan smoothly
+			//before being interrupted by the draw call
 			thesePolygons.push(polygon);
-			drawPolygonsOnMap(thesePolygons,latCenter,lngCenter);
+			setTimeout(function(){
+				drawPolygonsOnMap(thesePolygons,latCenter,lngCenter)
+			},100);
 		}
 		else alert("We tried to ask the server, but something went wrong!");
 	}
 	
-	//draw the polygons on the map
+	/// 3 - draw the polygons on the map
+	/// Draw the polygons
 	var drawPolygonsOnMap = function (polygons,latCenter,lngCenter){
 		
 		if(!google.maps) throw "Google Maps API not loaded!";
-		
+				
 		//clear the existing polygons (if any are drawn on the map)
 		currentPolygons.forEach(function(polyToClear){
 			if(polyToClear instanceof google.maps.Polygon){
@@ -89,9 +99,6 @@ gMap.load(function () {
 		currentPolygons.forEach(function(polyToDraw){
 			polyToDraw.setMap(map);
 		});
-		
-		//pan the map to the center coordinates
-		map.panTo(new google.maps.LatLng(latCenter, lngCenter));
 	}	
 });
 
